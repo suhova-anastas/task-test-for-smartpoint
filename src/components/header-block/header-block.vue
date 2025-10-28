d3408b8b8c5cbbe5c457852531a38e37fff37789
Анастасия Сухова <anastasiya.suhova.05@mail.ru>
2025-10-29 00:02:06 +0300

    Промежуточный этап: сделала хедер, загрузила таблицу с помощью Axios и PrimeVue(компонент DataTable)

--- src/components/header-block/header-block.vue
+++ src/components/header-block/header-block.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="header">
-        <RouterLink to="/users">Users</RouterLink>
-        <RouterLink to="/favorites">Favorites</RouterLink>
+        <div class="header__container">
+            <div class="header__navigation">
+                <RouterLink to="/users" class="header__navigation_item">
+                    <div class="header__navigation_item-icon pi pi-clipboard"></div>
+                    <p class="header__navigation_item-text">Пользователи</p>
+                </RouterLink>
+                <RouterLink to="/favorites" class="header__navigation_item">
+                    <div class="header__navigation_item-icon pi pi-heart"></div>
+                    <p class="header__navigation_item-text">Избранное</p>
+                </RouterLink>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import 'primeicons/primeicons.css'
 </script>
 
 <style src="./header-block.less" lang="less" />
--- src/components/users-block/users-block.vue
+++ src/components/users-block/users-block.vue
@@ -1,7 +1,30 @@
 <template>
-    <div class="users">USERSUSERSUSERSUSERSUSERSUSERSUSERSUSERSUSERS</div>
+    <div class="users">
+        <div class="users__container">
+            <DataTable :value="users">
+                <Column field="id" header="ID" />
+                <Column field="name" header="Name" />
+                <Column field="email" header="Email" />
+                <Column field="adress.street" header="Street" />
+                <Column field="phone" header="Phone" />
+                <Column field="company.name" header="Company" />
+            </DataTable>
+        </div>
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import axios from 'axios';
+
+const users = ref([])
+
+onMounted(async () => {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+    users.value = res.data
+})
+</script>
 
 <style src="./users-block.less" lang="less" />
